028ed66fa78e8d756fa1d0a60aa2921e1dc33ee1
wtu365 <130800254+wtu365@users.noreply.github.com>
2023-11-17 21:12:48 -0800

    Update PacmanWindow.java

--- Pacman/src/game/PacmanWindow.java
+++ Pacman/src/game/PacmanWindow.java
@@ -7,24 +7,42 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-// Class to create the window in which the game will be played in.
+/**
+ * The PacmanWindow class initializes the window in which the Pacman Shooter
+ * application will be played in.
+ * 
+ * @author Wesley Tu
+ */
 public class PacmanWindow extends JFrame {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -2251845675309884097L;
-
+	
+	/**
+	 * Defines the panel in which the elements of the game will be played in,
+	 * plus the restart button to restart the game.
+	 */
 	private StartMenu menu;
 	private JButton restart;
 	
+	/**
+	 * Default width and height of the window, plus the title of the game.
+	 */
 	public static final int DEFAULT_WIDTH = 500;
 	public static final int DEFAULT_HEIGHT = 500;
 	public static final String DEFAULT_TITLE = "Pacman Shooter";
 	
+	/**
+	 * Default constructor
+	 */
 	public PacmanWindow() {
 		this(DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_TITLE);
 	}
 	
+	/**
+	 * Constructor to create the window for the Pacman Shooter, plus add the restart button.
+	 * @param width
+	 * @param height
+	 * @param title
+	 */
 	public PacmanWindow(int width, int height, String title)
 	{
 		
@@ -50,6 +68,9 @@ public class PacmanWindow extends JFrame {
 		this.setVisible(true);	
 	}
 	
+	/**
+	 * Method that restarts the game if the restart button is pressed.
+	 */
 	public void start() {
 		menu.removeAll();
 		this.remove(menu);
