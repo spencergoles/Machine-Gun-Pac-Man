55fbd6b44f927242cec0695b55dc13a2f2f13fa5
wtu365 <130800254+wtu365@users.noreply.github.com>
2023-11-17 21:08:46 -0800

    Update Left_Ghost.java

--- Pacman/src/game/Left_Ghost.java
+++ Pacman/src/game/Left_Ghost.java
@@ -1,8 +1,21 @@
 package game;
 
+/**
+ * Left Ghost class that creates the ghost enemy that moves from right to left.
+ * @author Wesley Tu
+ */
 public class Left_Ghost extends Sprite {
+	
+	/**
+	 * Variable that keeps track of the rightmost edge of the window.
+	 */
 	private int INITIAL_X = 568;
 	
+	/**
+	 * Constructor for the Left Ghost entity.
+	 * @param x
+	 * @param y
+	 */
 	public Left_Ghost(int x, int y) {
 		super(x, y);
 		
@@ -10,6 +23,10 @@ public class Left_Ghost extends Sprite {
 		getImageDimensions();
 	}
 	
+	/**
+	 * Moves the Left Ghost entity from right to left, moving the entity back to
+	 * the right side of the screen once it touches the left side.
+	 */
 	public void move() {
 		if (x < 0)
 			x = INITIAL_X;
